0660611eb63ee8cf1cdfb18e5dec684f0fc98a60
Vikrant Kumar <vikrant@zeeve.io>
2022-05-07 11:52:55 +0530

    code updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World! vikrant kumar')
+  res.send('Hello World! vikrant kumar Devops')
 })
 
 //Launch listening server on port 3000
